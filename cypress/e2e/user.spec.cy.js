65b62331cec6ba2489be9f87c9ebb40167437fa9
Gabriel <gabrielhl25@hotmail.com>
2025-06-17 13:47:17 -0300

    Add user info update test with selectors for improved maintainability

---
+++ cypress/e2e/user.spec.cy.js
@@ -0,0 +1,41 @@
+import userData from '../fixtures/users/userData.json'
+
+describe('Orange HRM Tests', () => {
+
+  const selectorsList = {
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
+    loginButton: "[type='submit']",
+    dashboardGrid: '.orangehrm-dashboard-grid',
+    loginErrorAlert: "[role='alert']",
+    myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
+    firstNameField: "[name='firstName']",
+    middleNameField: "[name='middleName']",
+    lastNameField: "[name='lastName']",
+    genericField: ".oxd-input--active",
+    dateField: "[placeholder='yyyy-dd-mm']",
+    dateCloseButton: ".--close",
+    submitButton: "[type='submit']",
+  }
+  
+  it.only('User Info Update - Success', () => {
+    cy.visit('auth/login')
+    cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
+    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
+    cy.get(selectorsList.loginButton).click()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get(selectorsList.dashboardGrid)
+    cy.get(selectorsList.myInfoButton).click()
+    cy.get(selectorsList.firstNameField).clear().type('Test First Name')
+    cy.get(selectorsList.middleNameField).clear().type('Test Middle Name')
+    cy.get(selectorsList.lastNameField).clear().type('Test Last Name')
+    cy.get(selectorsList.genericField).eq(3).clear().type('EmployeeId')
+    cy.get(selectorsList.genericField).eq(4).clear().type('Test Other Id')
+    cy.get(selectorsList.genericField).eq(5).clear().type('Test Driver License Number')
+    cy.get(selectorsList.dateField).eq(0).clear().type('2026-01-01')
+    cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.submitButton).eq(0).click()
+    cy.get('body').should('contain', 'Successfully Updated')
+    cy.get('.oxd-toast-close-container')
+  })
+})
