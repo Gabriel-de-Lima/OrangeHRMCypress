833c913815c2da1e20e1f2626386001439b8a6a4
Gabriel <gabrielhl25@hotmail.com>
2025-06-23 11:11:17 -0300

    Enhance user info update test by adding selectors for generic comboboxes and gender field, and include comments for clarity

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -16,6 +16,9 @@ describe('Orange HRM Tests', () => {
     dateField: "[placeholder='yyyy-dd-mm']",
     dateCloseButton: ".--close",
     submitButton: "[type='submit']",
+    genericCombobox: ".oxd-select-wrapper",
+    comboboxDropdown: ".oxd-select-dropdown.--positon-bottom",
+    genderField: ".oxd-radio-wrapper"
   }
   
   it.only('User Info Update - Success', () => {
@@ -29,11 +32,18 @@ describe('Orange HRM Tests', () => {
     cy.get(selectorsList.firstNameField).clear().type('Test First Name')
     cy.get(selectorsList.middleNameField).clear().type('Test Middle Name')
     cy.get(selectorsList.lastNameField).clear().type('Test Last Name')
-    cy.get(selectorsList.genericField).eq(3).clear().type('EmployeeId')
-    cy.get(selectorsList.genericField).eq(4).clear().type('Test Other Id')
-    cy.get(selectorsList.genericField).eq(5).clear().type('Test Driver License Number')
-    cy.get(selectorsList.dateField).eq(0).clear().type('2026-01-01')
+    cy.get(selectorsList.genericField).eq(3).clear().type('EmployeeId') // Employee Id
+    cy.get(selectorsList.genericField).eq(4).clear().type('Test Other Id') // Other Id
+    cy.get(selectorsList.genericField).eq(5).clear().type('Test Driver License Number') // Driver License Number
+    cy.get(selectorsList.dateField).eq(0).clear().type('2026-01-01') // License Expiry Date
     cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.genericCombobox).eq(0).click()
+    cy.get(selectorsList.comboboxDropdown).contains('Belgian').click() // Nationality
+    cy.get(selectorsList.genericCombobox).eq(1).click()
+    cy.get(selectorsList.comboboxDropdown).contains('Married').click() // Marital Status
+    cy.get(selectorsList.dateField).eq(1).clear().type('2000-01-01') // Date of Birth
+    cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.genderField).eq(1).click()
     cy.get(selectorsList.submitButton).eq(0).click()
     cy.get('body').should('contain', 'Successfully Updated')
     cy.get('.oxd-toast-close-container')
