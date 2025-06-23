3ef183e71c319c27c7782b78ac172485e77fe456
Gabriel <gabrielhl25@hotmail.com>
2025-06-23 14:24:26 -0300

    Implement MyInfoPage class for improved user info update test structure and maintainability

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -2,50 +2,23 @@ import userData from '../fixtures/users/userData.json'
 import LoginPage from '../pages/loginPage'
 import DashboardPage from '../pages/dashboardPage'
 import NavbarPage from '../pages/navbarPage'
+import MyInfoPage from '../pages/myInfoPage'
 
 const loginPage = new LoginPage()
 const dashboardPage = new DashboardPage()
 const navbarPage = new NavbarPage()
+const myInfoPage = new MyInfoPage()
 
 describe('Orange HRM Tests', () => {
-
-  const selectorsList = {
-    
-    firstNameField: "[name='firstName']",
-    middleNameField: "[name='middleName']",
-    lastNameField: "[name='lastName']",
-    genericField: ".oxd-input--active",
-    dateField: "[placeholder='yyyy-mm-dd']",
-    dateCloseButton: ".--close",
-    submitButton: "[type='submit']",
-    genericCombobox: ".oxd-select-wrapper",
-    comboboxDropdown: ".oxd-select-dropdown.--positon-bottom",
-    genderField: ".oxd-radio-wrapper"
-  }
-  
   it('User Info Update - Success', () => {
     loginPage.accessLoginPage()
     loginPage.loginWithAnyUser(userData.userSuccess.username, userData.userSuccess.password)
     dashboardPage.checkDashboardPage()
     navbarPage.goToMyInfo()
-
-    cy.get(selectorsList.firstNameField).clear().type('Test First Name')
-    cy.get(selectorsList.middleNameField).clear().type('Test Middle Name')
-    cy.get(selectorsList.lastNameField).clear().type('Test Last Name')
-    cy.get(selectorsList.genericField).eq(3).clear().type('EmployeeId') // Employee Id
-    cy.get(selectorsList.genericField).eq(4).clear().type('Test Other Id') // Other Id
-    cy.get(selectorsList.genericField).eq(5).clear().type('Test Driver License Number') // Driver License Number
-    cy.get(selectorsList.dateField).eq(0).clear().type('2026-01-01') // License Expiry Date
-    cy.get(selectorsList.dateCloseButton).click()
-    cy.get(selectorsList.genericCombobox).eq(0).click()
-    cy.get(selectorsList.comboboxDropdown).contains('Belgian').click() // Nationality
-    cy.get(selectorsList.genericCombobox).eq(1).click()
-    cy.get(selectorsList.comboboxDropdown).contains('Married').click() // Marital Status
-    cy.get(selectorsList.dateField).eq(1).clear().type('2000-01-01') // Date of Birth
-    cy.get(selectorsList.dateCloseButton).click()
-    cy.get(selectorsList.genderField).eq(1).click()
-    cy.get(selectorsList.submitButton).eq(0).click()
-    cy.get('body').should('contain', 'Successfully Updated')
-    cy.get('.oxd-toast-close-container')
+    myInfoPage.checkMyInfoPage()
+    myInfoPage.fillNames('Test First Name', 'Test Middle Name', 'Test Last Name')
+    myInfoPage.fillEmployeeDetails('EmployeeId', 'Test Other Id', 'Test Driver License Number', '2026-01-01')
+    myInfoPage.fillOtherDetails('Belgian', 'Married', '2000-01-01', 'Female')
+    myInfoPage.submitForm()
   })
 })
---
+++ cypress/pages/myInfoPage.js
@@ -0,0 +1,62 @@
+class MyInfoPage {
+    selectors() {
+        const selectorsList = {
+            firstNameField: "[name='firstName']",
+            middleNameField: "[name='middleName']",
+            lastNameField: "[name='lastName']",
+            genericField: ".oxd-input--active",
+            dateField: "[placeholder='yyyy-mm-dd']",
+            dateCloseButton: ".--close",
+            submitButton: "[type='submit']",
+            genericCombobox: ".oxd-select-wrapper",
+            comboboxDropdown: ".oxd-select-dropdown.--positon-bottom",
+            genderField: ".oxd-radio-wrapper",
+            closeNotification: ".oxd-toast-close-container",
+        }
+        return selectorsList;
+    };
+
+    checkMyInfoPage() {
+        cy.location('pathname').should('match', /\/web\/index\.php\/pim\/viewPersonalDetails\/.*/); // Ensure we are on the My Info page, allowing any text after the slash
+        cy.get(this.selectors().firstNameField).should('be.visible'); // Check if first name field is visible
+        cy.get(this.selectors().middleNameField).should('be.visible'); // Check if middle name field is visible
+        cy.get(this.selectors().lastNameField).should('be.visible'); // Check if last name field is visible
+    }
+
+    fillNames(firstName, middleName, lastName) {
+        cy.get(this.selectors().firstNameField).clear().type(firstName); // Clear and type first name
+        cy.get(this.selectors().middleNameField).clear().type(middleName); // Clear and type middle name
+        cy.get(this.selectors().lastNameField).clear().type(lastName); // Clear and type last name
+    }
+
+    fillEmployeeDetails(employeeId, otherId, driverLicenseNumber, licenseExpiry) {
+        cy.get(this.selectors().genericField).eq(3).clear().type(employeeId); // Employee Id
+        cy.get(this.selectors().genericField).eq(4).clear().type(otherId); // Other Id
+        cy.get(this.selectors().genericField).eq(5).clear().type(driverLicenseNumber); // Driver License Number
+        cy.get(this.selectors().dateField).eq(0).clear().type(licenseExpiry); // License Expiry Date
+        cy.get(this.selectors().dateCloseButton).click();
+    }
+
+    fillOtherDetails(nationality, maritalStatus, dateOfBirth, gender) {
+        cy.get(this.selectors().genericCombobox).eq(0).click()
+        cy.get(this.selectors().comboboxDropdown).contains(nationality).click() // Nationality
+        cy.get(this.selectors().genericCombobox).eq(1).click()
+        cy.get(this.selectors().comboboxDropdown).contains(maritalStatus).click() // Marital Status
+        cy.get(this.selectors().dateField).eq(1).clear().type(dateOfBirth) // Date of Birth
+        cy.get(this.selectors().dateCloseButton).click()
+        if(gender == 'Male'){
+            cy.get(this.selectors().genderField).eq(0).click() // Select Male Option
+        }else if(gender == 'Female'){
+            cy.get(this.selectors().genderField).eq(1).click() // Select Female Option
+        }
+    }
+
+    submitForm() {
+        cy.get(this.selectors().submitButton).eq(0).click(); // Click the submit button
+        cy.get('body').should('contain', 'Successfully Updated'); // Check for success message
+        cy.get(this.selectors().closeNotification).click(); // Close the update notification
+    }
+    
+}
+
+export default MyInfoPage;
